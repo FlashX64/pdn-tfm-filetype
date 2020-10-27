88b381db32ecf85f5d026bfd0df23fc573ae29a2
FlashX64 <46058525+FlashX64@users.noreply.github.com>
2020-10-27 13:20:11 +0100

    Updated AssemblyInfo

--- TFMFileType/Properties/AssemblyInfo.cs
+++ TFMFileType/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Obecné informace o sestavení se řídí přes následující 
-// sadu atributů. Změnou hodnot těchto atributů se upraví informace
-// přidružené k sestavení.
 [assembly: AssemblyTitle("TFMFileType")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
-// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
-// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
 [assembly: ComVisible(false)]
 
-// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
 [assembly: Guid("77c06975-8457-4cd7-ae4d-3b2e31264e9c")]
 
-// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
-//
-//      Hlavní verze
-//      Podverze
-//      Číslo sestavení
-//      Revize
-//
-// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
-// pomocí zástupného znaku * takto:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
